5f2d236d26dad3bc2c002d413753d8bba0eeef73
NurKeinNeid <mralexman3000@gmail.com>
2022-11-22 23:38:30 +0100

    msm8998-common: Add hotword enrollment from taimen
    Change-Id: I5dcf73894050079c9493119befb017e6026b634c
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- common.mk
+++ common.mk
@@ -4,6 +4,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+$(call inherit-product, vendor/oneplus/google/google.mk)
 $(call inherit-product, vendor/oneplus/msm8998-common/msm8998-common-vendor.mk)
 
 # APEX
