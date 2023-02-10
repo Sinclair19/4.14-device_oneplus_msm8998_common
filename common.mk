a05f2f26ad017acf0536f3fae0ddc79a5d4a0e54
NurKeinNeid <mralexman3000@gmail.com>
2023-02-10 23:46:56 +0100

    [DNM] msm8998-common: Inherit DeviceSettings from msmnile
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- common.mk
+++ common.mk
@@ -144,6 +144,10 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     vendor.qti.hardware.camera.device@1.0
 
+# Device Settings
+PRODUCT_PACKAGES += \
+    DeviceSettings
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-impl \
